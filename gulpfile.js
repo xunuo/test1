a19d6937e053a653759c79c5582d709a786b94b1
nuo.xun <nuo.xun@alibaba-inc.com>
2015-05-05 22:08:23 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
 
-gulp.task('publish-page', function() {
+gulp.task('publish-page', ['publish-page-cn'], function() {
      return gulp.src('./pages/**/*')
      .pipe(ghPages());
+});
+
+gulp.task('publish-page-cn', function() {
+     return gulp.src('./pages/**/*')
+     .pipe(ghPages({
+     		remoteUrl : 'git@gitcafe.com:xunuo/test.git'
+     }));
 });
